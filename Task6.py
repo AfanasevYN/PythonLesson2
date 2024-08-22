136e88cf226389d23c70d393f3752cf654167c9a
AfanasevYN <Kwice2@yandex.ru>
2024-08-22 23:54:20 +0300

    Add files via upload

---
+++ Task6.py
@@ -0,0 +1,6 @@
+Home_work = 12
+Time = 1.5
+Class = 'Python'
+One = Time / Home_work
+
+print('Курс:',Class,',', 'всего задач:',Home_work,',' ,'затрачено часов:',Time,',','среднее время выполнени',One,'часа.')
